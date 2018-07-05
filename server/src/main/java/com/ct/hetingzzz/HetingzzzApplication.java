a17825d52e98b30f046649f2c2df50a622cd46ad
a7902161 <617788219@qq.com>
2018-07-05 22:40:55 +0800

    创建项目

---
+++ server/src/main/java/com/ct/hetingzzz/HetingzzzApplication.java
@@ -0,0 +1,12 @@
+package com.ct.hetingzzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HetingzzzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HetingzzzApplication.class, args);
+    }
+}
