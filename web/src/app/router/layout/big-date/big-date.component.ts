150b1f3a7117da6ecf46b10b3faf244a07fc0635
taoshen <617788219@qq.com>
2018-08-20 18:09:52 +0800

    日常提交

--- web/src/app/router/layout/big-date/big-date.component.ts
+++ web/src/app/router/layout/big-date/big-date.component.ts
@@ -8,7 +8,8 @@ import * as moment from 'moment';
 })
 export class BigDateComponent implements OnInit {
 
-  dates: Array<any> = [{name: '恋爱日', date: moment('2017-09-13 00:00:00').format('YYYY-MM-DD')},
+  dates: Array<any> = [
+    {name: '恋爱日', date: moment('2017-09-13 00:00:00').format('YYYY-MM-DD')},
     {name: '第一次亲亲', date: moment('2017-10-22 00:00:00').format('YYYY-MM-DD')},
     {name: '亻可月巴女亭生日', date: moment('1965-11-09 00:00:00').format('YYYY-MM-DD')},
     {name: '亻可月巴女亭把我礼物弄丢', date: moment('2018-07-22 00:00:00').format('YYYY-MM-DD')}
