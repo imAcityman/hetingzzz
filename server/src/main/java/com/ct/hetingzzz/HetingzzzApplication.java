1fbe655471027f8a3c8fdcede727b10475b2ab92
taoshen <617788219@qq.com>
2018-08-22 14:54:58 +0800

    日常提交

--- server/src/main/java/com/ct/hetingzzz/HetingzzzApplication.java
+++ server/src/main/java/com/ct/hetingzzz/HetingzzzApplication.java
@@ -2,9 +2,16 @@ package com.ct.hetingzzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HetingzzzApplication {
+public class HetingzzzApplication  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HetingzzzApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HetingzzzApplication.class, args);
