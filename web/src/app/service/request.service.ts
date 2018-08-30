c4ad938d1eaf0621e4052b65191d67131a485ba6
taoshen <617788219@qq.com>
2018-08-30 10:38:56 +0800

    日常提交

--- web/src/app/service/request.service.ts
+++ web/src/app/service/request.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {MyStorageService} from './my.storage.service';
+import {timeout} from 'rxjs/operators/timeout';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class RequestService {
   }
 
   get(url: string, params: HttpParams): Observable<any> {
-    return this.http.get(url, {params: params});
+    return this.http.get(url, {params: params}).pipe(timeout(30000));
   }
 
   post(url: string, params: HttpParams): Observable<any> {
-    return this.http.post(url, {}, {params: params});
+    return this.http.post(url, {}, {params: params}).pipe(timeout(30000));
   }
 }
