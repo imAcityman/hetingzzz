b73c664af6fb6ffcd76e3ab9867e504f09f62fc0
taoshen <617788219@qq.com>
2018-09-29 19:39:11 +0800

    日常提交

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+document.getElementById('preload').style.display = 'none';
 if (environment.production) {
   enableProdMode();
 }
