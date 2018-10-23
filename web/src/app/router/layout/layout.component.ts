e7cadf90f1c983429dcc388abb5ea175f819104b
taoshen <617788219@qq.com>
2018-10-23 09:47:32 +0800

    日常提交

--- web/src/app/router/layout/layout.component.ts
+++ web/src/app/router/layout/layout.component.ts
@@ -12,11 +12,11 @@ import {animate, group, query, style, transition, trigger} from '@angular/animat
   animations: [trigger('routeAnimation', [
     transition('* => *', [
       query(':leave', style({position: 'absolute', width: '100%', height: '100%'}), {optional: true}),
-      query(':enter', style({position: 'absolute', width: '100%', height: '100%'}), {optional: true}),
+      query(':enter', style({position: 'absolute', width: '100%', height: '100%', transform: 'translateX(100%)'}), {optional: true}),
 
       group([
         query(':leave', animate('.3s ease-in-out', style({transform: 'translateX(-100%)'})), {optional: true}),
-        query(':enter', animate('.3s ease-in-out', style({transform: 'translateX(0)'})), {optional: true})
+        query(':enter', animate('.3s ease-in-out', style({transform: 'translateX(0)', 'z-index': 2})), {optional: true})
       ])
     ])
   ])]
