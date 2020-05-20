7f92ae2a1212e005644509cd2dd6725edfb818a1
chentao <chent@ijia120.com>
2020-05-20 16:44:18 +0800

    日常提交

--- server/src/main/java/com/ct/hetingzzz/HetingzzzApplication.java
+++ server/src/main/java/com/ct/hetingzzz/HetingzzzApplication.java
@@ -2,18 +2,11 @@ package com.ct.hetingzzz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class HetingzzzApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(HetingzzzApplication.class);
-    }
+public class HetingzzzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HetingzzzApplication.class, args);
